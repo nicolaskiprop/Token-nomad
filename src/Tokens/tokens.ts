b06ed4875040c44b5f1b38d91455660e45e83490
nicolaskiprop <yegonicholas@gmail.com>
2022-11-17 15:47:36 +0300

    eigenphi token

--- src/Tokens/tokens.ts
+++ src/Tokens/tokens.ts
@@ -1,1 +1,37 @@
+import axios from "axios";
 
+export const fetchHotLiquidityPools = async() => {
+    try {
+        const { data } = await axios({
+            method:'get',
+            url:'https://eigenphi.io/api/v2/arbitrage/stat/lp/hotLp/?chain=ethereum&pageSize=1000&period=7&sortBy=volume'
+        })
+        const hotPools = data.result
+        console.log(hotPools);
+        let tokenData: any ={}
+        let pools: any =[]
+
+        // Format data to caapture what is required
+        // const dbPools = hotPools.map((pool: any) =>{
+        //     const lpAddress = pool.lpAddress;
+        //     const tokens = pool.tokens;
+
+        //     const token0 = tokens[0].address
+        //     const token1 = tokens[1].address
+
+        //     tokenData[token0] = tokens[0]
+        //     tokenData[token1] = tokens[1]
+
+        //     pools.push([token0, token1])
+
+        //     return {lpAddress, token0, token1}
+
+        // })
+
+        // return { pools, dbPools, tokenData }
+        
+    } catch (error) {
+        console.log(`error :${error}`);
+        
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-const main = () =>{
+import { fetchHotLiquidityPools } from "./Tokens/tokens";
+
+const main = async() =>{
 console.log('***********Starting the arbitrage bot******************');
 try {
+    const poolData =  await fetchHotLiquidityPools()
+    console.log(poolData);
     
+    // const tokenData = poolData?.tokenData
+    // const poolToSave = poolData?.dbPools
+    // let pools = poolData?.pools
+    // console.log('pools fetched', pools.length);
 } catch (error) {
     console.log(`error : ${error}`);  
+    
+
 }
 }
 main()

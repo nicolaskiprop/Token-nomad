b8b877346f3ec664d71b7db47411a5ad9e7c6ba3
nicolaskiprop <yegonicholas@gmail.com>
2022-11-18 23:29:29 +0300

    eigenphi data

--- src/Tokens/tokens.ts
+++ src/Tokens/tokens.ts
@@ -8,28 +8,7 @@ export const fetchHotLiquidityPools = async() => {
         })
         const hotPools = data.result
         console.log(hotPools);
-        let tokenData: any ={}
-        let pools: any =[]
-
-        // Format data to caapture what is required
-        // const dbPools = hotPools.map((pool: any) =>{
-        //     const lpAddress = pool.lpAddress;
-        //     const tokens = pool.tokens;
-
-        //     const token0 = tokens[0].address
-        //     const token1 = tokens[1].address
-
-        //     tokenData[token0] = tokens[0]
-        //     tokenData[token1] = tokens[1]
-
-        //     pools.push([token0, token1])
-
-        //     return {lpAddress, token0, token1}
-
-        // })
-
-        // return { pools, dbPools, tokenData }
-        
+         
     } catch (error) {
         console.log(`error :${error}`);
         
